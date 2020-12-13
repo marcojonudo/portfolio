70fdea6489282bcb5d57bb622159701bd6decfe0
Marco <marco.martinez@feuvert.es>
2020-12-13 23:09:00 +0100

    Add about me content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {NotificationService} from './services/notification.service';
 import {Subscription} from 'rxjs';
 import {Section} from './objects/sections/section';
-import {WelcomeSection} from './objects/sections/welcome-section';
 import {Constants} from './objects/constants';
 import {User} from './objects/users/user';
-import {DevUser} from './objects/users/dev-user';
 import {Style} from './objects/style';
+import {NormalUser} from './objects/users/normal-user';
+import {WelcomeSection} from './objects/sections/welcome-section';
 
 @Component({
     selector: 'app-root',
@@ -27,7 +27,7 @@ export class AppComponent implements OnInit {
     styles: Style[];
 
     constructor() {
-        this.user = new DevUser();
+        this.user = new NormalUser();
         this.section = new WelcomeSection();
         this.styles = [];
 

7db278699d8519e502528228b3239019d511bf01
Marco <marco.martinez@feuvert.es>
2022-04-03 00:43:02 +0200

    Change version section structure
    Now, cards are used to display the image and the text.

--- client/src/app/components/home/about/about.component.ts
+++ client/src/app/components/home/about/about.component.ts
@@ -1,5 +1,15 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+	AfterViewInit,
+	ChangeDetectionStrategy,
+	ChangeDetectorRef,
+	Component,
+	ElementRef,
+	Input,
+	ViewChild
+} from '@angular/core';
 import { User } from '../../../objects/users/user';
+import { fromEvent } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-about',
@@ -7,8 +17,48 @@ import { User } from '../../../objects/users/user';
 	styleUrls: ['./about.component.sass'],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit {
 
 	@Input() user: User;
+
+	private readonly LEFT_CARD_UP = 'left-card-up';
+	private readonly LEFT_CARD_DOWN = 'left-card-down';
+	private readonly RIGHT_CARD_UP = 'right-card-up';
+	private readonly RIGHT_CARD_DOWN = 'right-card-down';
+
+	@ViewChild('pictureWrapper') pictureWrapper: ElementRef;
+	@ViewChild('textWrapper') textWrapper: ElementRef;
+
+	pictureWrapperUp: boolean;
+	pictureWrapperClass: string;
+	textWrapperClass: string;
+
+	constructor(private cdRef: ChangeDetectorRef) {
+		this.pictureWrapperUp = true;
+	}
+
+	ngAfterViewInit(): void {
+		fromEvent(this.pictureWrapper.nativeElement, 'mouseover').pipe(
+			tap(() => {
+				if (!this.pictureWrapperUp) {
+					this.toggleCardAnimations(this.LEFT_CARD_UP, this.RIGHT_CARD_DOWN);
+				}
+			})
+		).subscribe();
+		fromEvent(this.textWrapper.nativeElement, 'mouseover').pipe(
+			tap(() => {
+				if (this.pictureWrapperUp) {
+					this.toggleCardAnimations(this.LEFT_CARD_DOWN, this.RIGHT_CARD_UP);
+				}
+			})
+		).subscribe();
+	}
+
+	toggleCardAnimations(leftCardClass: string, rightCardClass: string): void {
+		this.pictureWrapperClass = leftCardClass;
+		this.textWrapperClass = rightCardClass;
+		this.pictureWrapperUp = !this.pictureWrapperUp;
+		this.cdRef.detectChanges();
+	}
 
 }

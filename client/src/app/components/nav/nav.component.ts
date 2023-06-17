2f0e758ee64267998a4d923f42607c4f403d470a
Marco <marco.martinez@feuvert.es>
2023-06-17 14:17:54 +0200

    Always navigate to section path

--- client/src/app/components/nav/nav.component.ts
+++ client/src/app/components/nav/nav.component.ts
@@ -104,9 +104,7 @@ export class NavComponent {
 	}
 
 	selectSection(section: Section, url: string): void {
-		if (this.navService.path() !== Constants.URL.HOME || section.type === this.welcomeSection.type) {
-			this.navigateTo(url);
-		}
+		this.navigateTo(url);
 		if (section.type !== this.welcomeSection.type) {
 			this.toggleOpened();
 		}
